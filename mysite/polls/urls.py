12a8a3f23aa92deb9c0b2cec11232f528f2d4401
Paulo Vítor <paulovmedeiros@outlook.com>
2019-12-02 14:44:23 -0300

    parte 3 do tutorial concluída

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
+from django.http import HttpResponse
 from . import views
+from .models import Question
 
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name="detail"),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('specifics/<int:question_id>/', views.detail, name='detail'),
 ]

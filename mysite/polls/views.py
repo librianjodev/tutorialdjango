40421728b6188d56c73e8b1cd1fe50fc50a8c870
Paulo Vítor <paulovmedeiros@outlook.com>
2019-12-02 21:06:16 -0300

    parte 5 do tutorial concluída

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -35,3 +36,8 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def get_queryset(self):
+    return Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
